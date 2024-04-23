e18d6cf39188f110ba4df85bb9b0b61e51e41fc4
JoNee <hhcho1234@naver.com>
2024-04-23 15:39:54 +0900

    MEMBER 틀잡기, DTO 생성 MAPPER 연결

--- CafeProject/src/main/java/edu/kh/semi/member/controller/MemberController.java
+++ CafeProject/src/main/java/edu/kh/semi/member/controller/MemberController.java
@@ -1,15 +1,35 @@
 package edu.kh.semi.member.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import edu.kh.semi.member.dto.Member;
+import edu.kh.semi.member.model.service.MemberService;
+import lombok.RequiredArgsConstructor;
 
 
 @Controller
-@RequestMapping("member") 
+@RequestMapping("member")
+@RequiredArgsConstructor
 public class MemberController {
+	private final MemberService service;
+	
 	@GetMapping("login")
 	public String login() {
 		return "board/Login";
+	}
+	
+	@PostMapping("login")
+	public String login(
+			Member inputMember,
+			RedirectAttributes ra,
+			Model model) {
+		
+		Member loginMember = service.login(inputMember); 
+		return null;
 	}
 }
---
+++ CafeProject/src/main/java/edu/kh/semi/member/model/service/MemberService.java
@@ -0,0 +1,13 @@
+package edu.kh.semi.member.model.service;
+
+import edu.kh.semi.member.dto.Member;
+
+public interface MemberService {
+
+	/** 로그인 기능
+	 * @param inputMember : 로그인 시 입력한 이메일, 비밀번호
+	 * @return
+	 */
+	Member login(Member inputMember);
+
+}
