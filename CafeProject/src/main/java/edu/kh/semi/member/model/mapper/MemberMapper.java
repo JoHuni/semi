103b88c469e9a5d7f2d2114258f36a0de2ac6e24
JoNee <hhcho1234@naver.com>
2024-04-30 16:42:14 +0900

    이메일 인증 후 비밀번호 변경 구현 완료

--- CafeProject/src/main/java/edu/kh/semi/member/model/mapper/MemberMapper.java
+++ CafeProject/src/main/java/edu/kh/semi/member/model/mapper/MemberMapper.java
@@ -1,38 +1,59 @@
 package edu.kh.semi.member.model.mapper;
 
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import edu.kh.semi.member.model.dto.Member;
 
 @Mapper
-
 public interface MemberMapper {
 
-	/** 입력한 회원 정보 조회
-	 * @param memberEmail
-	 * @return memberEmail
-	 */
-	Member login(String memberEmail);
+   /** 입력한 회원 정보 조회
+    * @param memberEmail
+    * @return memberEmail
+    */
+   Member login(String memberEmail);
 
-	int signup(Member member);
+   int signup(Member member);
 
-	/** 사이드 메뉴에 나타나는 회원 수
-	 * @return 
-	 */
-	int countMember();
+   /** 사이드 메뉴에 나타나는 회원 수
+    * @return 
+    */
+   int countMember();
 
-	String getEncryptedPass(String memberEmail);
+   /** 아이디 찾기
+    * @param memberTel
+    * @return
+    */
+   String findId(String memberTel);
 
-	int checkPassRedundancy(Member member);
-	/** 아이디 찾기
-	 * @param memberTel
-	 * @return
-	 */
-	String findId(String memberTel);
+   /** 프로필 이미지 변경
+    * @param mem
+    * @return
+    */
+   int profile(Member mem);
 
-	int nickNameRedundancy(String memberNickname);
 
-	int emailRedundancy(String memberEmail);
+   String getEncryptedPass(String memberEmail);
+
+   int checkPassRedundancy(Member member);
+
+   int emailRedundancy(String memberEmail);
+
+   int nickNameRedundancy(String memberNickname);
+
+   String checkPw(Map<String, Object> map);
+
+   int changePw(Map<String, Object> map);
+
+   int withdrawal(int memberNo);
+
+   int findPw(String memberEmail);
+
+   int updatePw(String memberEmail);
+
+   int updatePw(Map<String, Object> map);
 
 
 }
